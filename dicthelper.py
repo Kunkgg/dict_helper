007bc131380529ac048139bdd213e2ae9fd29a83
gk07 <gk_520@hotmail.com>
2019-06-23 22:36:46 +0800

    Made electron GUI roughly(not include send to Anki function)

--- dicthelper.py
+++ dicthelper.py
@@ -3,7 +3,7 @@
 this module implements the dicthelper
  - search a selected English word in local dictionary and online dictionary
  - display searching result with tk GUI
- - store result into Anki with speical deckname and modlename 
+ - store result into Anki with speical deckname and modlename
 """
 
 import string
@@ -14,21 +14,38 @@ import urllib.request
 import urllib.error
 from xml.etree import ElementTree
 from pprint import pprint
+from collections import namedtuple
 
 from display import display
 
-### start:local dictinary(ENCN_Collins) file path ###
-data_path, _ = os.path.abspath(__file__).rsplit('/',1)
+# start:local dictinary(ENCN_Collins) file path ###
+data_path, _ = os.path.abspath(__file__).rsplit('/', 1)
 local_dict_path_wordforms = os.path.join(data_path, 'data/wordforms.json')
 local_dict_path_tags = os.path.join(data_path, 'data/tags.json')
 local_dict_path_edict = os.path.join(data_path, 'data/edict')
 edict_files = ['part1.json', 'part2.json', 'part3.json', 'part4.json']
-### end ###
+# end ###
 
 # command for getting the selected word on linux
 GET_WORD_COMMAND = 'xclip -selection primary -o'
+
+LOCAL_DICTIONARY = 'ENCN_Collins'
+ONLINE_DICTIONARY = 'YouDao'
+
 # online dictinary youdao API
-YOUDAO_API = 'http://dict.youdao.com/fsearch?client=deskdict&keyfrom=chrome.extension&pos=-1&doctype=xml&xmlVersion=3.2&dogVersion=1.0&vendor=unknown&appVer=3.1.17.4208&le=eng&q='
+YOUDAO_API_PART1 = 'http://dict.youdao.com/'
+YOUDAO_API_PART2 = 'fsearch?client=deskdict&keyfrom=chrome.extension&pos=-1'
+YOUDAO_API_PART3 = '&doctype=xml&xmlVersion=3.2&dogVersion=1.0&vendor=unknown'
+YOUDAO_API_PART4 = '&appVer=3.1.17.4208&le=eng&q='
+YOUDAO_API = (
+        YOUDAO_API_PART1 +
+        YOUDAO_API_PART2 +
+        YOUDAO_API_PART3 +
+        YOUDAO_API_PART4
+)
+
+Word = namedtuple('Word', ['expression', 'reading', 'glossary'])
+
 
 def get_selected_word():
     """get selected word from screen
@@ -36,16 +53,18 @@ def get_selected_word():
     :rtype: str
     """
     word = subprocess.Popen(
-                GET_WORD_COMMAND, 
-                shell=True, 
-                stdout=subprocess.PIPE).stdout.read()
+        GET_WORD_COMMAND,
+        shell=True,
+        stdout=subprocess.PIPE).stdout.read()
     word = word.decode('utf-8')
     return word.strip(string.punctuation)
 
+
 def search_local_dictionary(word):
-    """search in local dictionary(Collins English-Chinese), default analysising word root
+    """search in local dictionary(Collins English-Chinese), default
+    analysising word root
     :param word: str
-    :return: the result of search_local_dictionary 
+    :return: the result of search_local_dictionary
     :rtype: list(cantains all same root words in the local dictionary)
     Usage::
     In [2]: search_local_dictionary('test')
@@ -53,8 +72,11 @@ def search_local_dictionary(word):
     [['test',
     '/te̱st/',
     '',
-    ['v.检验；试验；测试<br> When you <b>test</b> something, you try it, for example by touching it or using it for a short time, in order to find out what it is, what condition it is in, or how well it works. ',
-    'n.试验；测试；检验<br> A <b>test</b> is a deliberate action or experiment to find out how well something works. ',
+    ['v.检验；试验；测试<br> When you <b>test</b> something, you try it, for example
+    by touching it or using it for a short time, in order to find out what it
+    is, what condition it is in, or how well it works. ',
+    'n.试验；测试；检验<br> A <b>test</b> is a deliberate action or experiment to find
+    out how well something works. ',
     ....]]]
     """
     search_words = [word]
@@ -66,9 +88,9 @@ def search_local_dictionary(word):
         wordforms = fp.readlines()
     wordforms = json.loads(''.join(wordforms))
     wordforms_res = (
-                wordforms.get(word, None) or 
-                wordforms.get(word.lower(), None)
-                )
+        wordforms.get(word, None) or
+        wordforms.get(word.lower(), None)
+    )
     if wordforms_res:
         search_words.extend(wordforms_res)
     # print(search_words)
@@ -84,15 +106,16 @@ def search_local_dictionary(word):
                 res = edict_part['defs'][index[0]]
                 if len(res) == 4:
                     del res[2]
+                res = Word(res[0], res[1], res[2])
                 result.append(res)
                 break
     return result
-            
+
 
 def search_online_dictionary(word):
     """search in online dictionary(YOUDAO_API)
     :param word: str
-    :return: the result of search_online_dictionary 
+    :return: the result of search_online_dictionary
     :rtype: list
     Usage::
     In [3]: search_online_dictionary('test')
@@ -113,21 +136,23 @@ def search_online_dictionary(word):
         tree = ElementTree.fromstring(html)
         try:
             expression = tree.find('return-phrase').text
-            us_phonetic_symbol = '/' + tree.find('us-phonetic-symbol').text + '/'
+            us_phonetic_symbol = '/' + \
+                tree.find('us-phonetic-symbol').text + '/'
             glossary = []
             for node in tree.iter('content'):
                 glossary.append(node.text)
         except AttributeError:
             return None
-        result = [expression, us_phonetic_symbol, glossary]
+        result = Word(expression, us_phonetic_symbol, glossary)
         return result
+
 
 def search(word):
     """search word in local and online dictionary
-    save local and online result in a dict 
+    save local and online result in a dict
     """
     result = {
-        'local': search_local_dictionary(word), 
+        'local': search_local_dictionary(word),
         'online': search_online_dictionary(word)
     }
     return result
@@ -137,7 +162,7 @@ def dicthelper():
     """entry"""
     word = get_selected_word()
     if word:
-        display(search(word))
+        display(search(word), LOCAL_DICTIONARY, ONLINE_DICTIONARY)
 
 
 def test():
@@ -149,5 +174,5 @@ def test():
 
 
 if __name__ == '__main__':
-    test()
-    # dicthelper()
+    # test()
+    dicthelper()
--- display.py
+++ display.py
@@ -1,162 +1,21 @@
-"""
-this module implements GUI
-"""
-import tkinter
-from tkinter import *
-from tkinter import ttk
+# -*- coding:utf-8 -*-
 
-from connectoAnki import addNote
-from connectoAnki import data_add_note
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-# format string
-LOCAL_FIRST_LINE = '{}\t{}'
-ONLINE_FIRST_LINE = 'YouDao\n\n{}\t{}'
+TEMPLATE_PATH = '.'
+TEMPLATE_FILENAME = 'base.html'
+DIS_FILE = './index.html'
 
-def on_configure(event):
-    # update scrollregion after starting 'mainloop'
-    # when all widgets are in canvas
-    canvas.configure(scrollregion=canvas.bbox('all'))
+LOCAL_DICTIONARY = 'ENCN_Collins'
+ONLINE_DICTIONARY = 'YouDao'
 
-# forge the root weight and entitle
-root = Tk()
-root.title("Dict Helper")
-# root.lift()
-
-# --- create canvas with scrollbar ---
-
-canvas = tkinter.Canvas(root, width=620)
-canvas.pack(side=tkinter.LEFT)
-
-scrollbar = tkinter.Scrollbar(root, command=canvas.yview)
-scrollbar.pack(side=tkinter.LEFT, fill='y')
-
-canvas.configure(yscrollcommand = scrollbar.set)
-
-# update scrollregion after starting 'mainloop'
-# when all widgets are in canvas
-canvas.bind('<Configure>', on_configure)
-
-# --- put frame in canvas ---
-
-mainframe = ttk.Frame(canvas, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-mainframe.columnconfigure(0, weight=1)
-mainframe.rowconfigure(0, weight=1)
-
-canvas.create_window((0,0), window=mainframe, anchor='nw')
-
-# grid row
-row = IntVar(value=1)
-
-def newNote(note):
-    """new note"""
-    fields = data_add_note['params']['note']['fields'].keys()
-    for field in fields:
-        data_add_note['params']['note']['fields'][field] = note.get(field, '')
-    return data_add_note
-
-def send_all_glossary(first_line, glossary):
-    """send all glossarys to Anki"""
-    note = note_all_glossary(first_line, glossary)
-    note = newNote(note)
-    # print(note)
-    addNote(note)
-    
-
-def send_single_glossary(first_line, glossary):
-    """send a speical glossary to Anki"""
-    note = note_single_glossary(first_line, glossary)
-    note = newNote(note)
-    # print(note)
-    addNote(note)
-
-def _expAndreading(first_line):
-    if first_line.startswith('YouDao'):
-        first_line = first_line[8:]
-    return first_line.split('\t')
-
-def note_all_glossary(first_line, answer):
-    """record all glossarys"""
-    exp, reading = _expAndreading(first_line)
-    return {
-        'expression': exp,
-        'reading': reading,
-        'glossary': answer[-1]
-    }
-    
-
-def note_single_glossary(first_line, glossary):
-    """record single glossarys"""
-    exp, reading = _expAndreading(first_line)
-    return {
-        'expression': exp,
-        'reading': reading,
-        'glossary': glossary
-    }
+env = Environment(loader=FileSystemLoader(TEMPLATE_PATH))
+template = env.get_template(TEMPLATE_FILENAME)
 
 
-def make_item(first_line, glossary):
-    """make glossary label and accordding Anki button"""
-    ttk.Label(
-        mainframe, 
-        text=glossary, 
-        wraplength=500).grid(column=1, row=row.get(), sticky=W)
-    ttk.Button(
-        mainframe, 
-        text="Anki", 
-        command=lambda: send_single_glossary(first_line, glossary)
-        ).grid(column=2, row=row.get(), sticky=E)
-
-def display(answer): 
-    """forge GUI"""
-
-    local_answers = answer.get('local')
-    online_answer = answer.get('online')
-    
-    for local_answer in local_answers:
-        if local_answer:
-            local_first_line = LOCAL_FIRST_LINE.format(local_answer[0],local_answer[1])
-            
-            row.set(row.get()+1)
-            ttk.Label(
-                mainframe, 
-                text=local_first_line, 
-                foreground='red',
-                font="-size 18").grid(column=1, row=row.get(), sticky=W)
-            ttk.Button(
-                    mainframe, 
-                    text="AlltoAnki", 
-                    command=lambda: send_all_glossary(local_first_line, local_answer)
-                    ).grid(column=2, row=row.get(), sticky=E)
-            for glossary in local_answer[-1]:
-                row.set(row.get()+1)
-                # local_first_line_sigle = local_first_line + '\t\t' + str(row.get())
-                # make_item(local_first_line_sigle, glossary)
-                make_item(local_first_line, glossary)
-    if online_answer:
-        row.set(row.get()+1)
-        online_first_line = ONLINE_FIRST_LINE.format(online_answer[0],online_answer[1])
-        ttk.Label(
-            mainframe, 
-            text=online_first_line, 
-            foreground='red',
-            font="-size 18").grid(column=1, row=row.get(), sticky=W)
-        ttk.Button(
-                mainframe, 
-                text="AlltoAnki", 
-                command=lambda: send_all_glossary(online_first_line, online_answer)
-                ).grid(column=2, row=row.get(), sticky=E)
-        
-        for glossary in online_answer[-1]:
-            row.set(row.get()+1)
-            # online_first_line_sigle = online_first_line + '\t\t' + str(row.get())
-            # make_item(online_first_line_sigle, glossary)
-            make_item(online_first_line, glossary)
-    for child in mainframe.winfo_children(): 
-        child.grid_configure(padx=5, pady=5)
-    # display position
-    positionRight = int(root.winfo_screenwidth()/2)
-    positionDown = 100
-    root.geometry("+{}+{}".format(positionRight, positionDown))
-    root.mainloop()
-
+def display(result, local_dictionary, online_dictionary):
+    html = template.render(result=result,
+                           local_dictionary=local_dictionary,
+                           online_dictionary=online_dictionary)
+    with open(DIS_FILE, 'w') as fp:
+        fp.writelines(html)
