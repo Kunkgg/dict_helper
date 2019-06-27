d96603a1b7e1a0bb6115ef2cba9e94c3fbcc61d3
gk07 <gk_520@hotmail.com>
2019-06-27 20:03:59 +0800

    add some comments for display.py

--- display.py
+++ display.py
@@ -1,13 +1,16 @@
 # -*- coding:utf-8 -*-
-
+"""
+Write the result of word searching in the special index.html for electron GUI.
+"""
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import os
 
-TEMPLATE_FILENAME = 'base.html'
 
+# the display file path
 dis_file = 'dict_helper-linux-x64/resources/app/index.html'
 root_dir, _ = os.path.abspath(__file__).rsplit('/', 1)
 DIS_FILE_PATH = os.path.join(root_dir, dis_file)
+TEMPLATE_FILENAME = 'base.html'
 
 LOCAL_DICTIONARY = 'ENCN_Collins'
 ONLINE_DICTIONARY = 'YouDao'
